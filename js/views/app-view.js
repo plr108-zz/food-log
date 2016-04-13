ed7203a30edbfa263ebb6446ab9c197994b1715a
Patrick Roche <patrick.l.roche@gmail.com>
2016-04-12 20:56:29 -0400

    Add getFoodItems() stub

--- js/views/app-view.js
+++ js/views/app-view.js
@@ -4,13 +4,25 @@ var app = app || {};
 
 app.AppView = Backbone.View.extend({
 
-	el: '#health-tracker-app',
+    el: '#health-tracker-app',
 
-	init: function() {
+    events: {
+        'click #search-button': 'getFoodItems'
+    },
 
-	},
+    init: function() {
 
-	render: function() {
-		
-	}
+    },
+
+    render: function() {
+
+    },
+
+    // Query the Nutritionix API using the value of the search input.
+    // Display the results (food items) 
+    getFoodItems: function() {
+
+        var queryString = $('#search-input').val();
+        console.log(queryString);
+    },
 });
