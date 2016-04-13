6bde7ad335643475be722bf195838c2726f8b166
Patrick Roche <patrick.l.roche@gmail.com>
2016-04-13 16:43:57 -0400

    fix typo in #food-item-template, add template to FoodItemView

--- js/views/food-item-view.js
+++ js/views/food-item-view.js
@@ -6,6 +6,9 @@ app.FoodItemView = Backbone.View.extend({
     // DOM element for food item
     tagName: 'li',
 
+    // food item template
+    template: _.template($('#food-item-template').html()),
+
     //food item DOM events will go here
     events: {
 
